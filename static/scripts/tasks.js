c9ad6e55ccbabd199b8dbed507ce0796c382c342
Lee-Junhee <jlee02@stuy.edu>
2020-01-07 21:51:15 -0500

    wrote code for async updates

--- static/scripts/tasks.js
+++ static/scripts/tasks.js
@@ -1,21 +1,45 @@
 //relies on jQuery being implemented on the html page
 
 const edittask = function(e) {
+	let id = this.id;
+	let stat = document.getElementById("status").value;
+	let content = document.getElementById("content").value;
+	let deadline = document.getElementById("deadline").value;
 	$.ajax({
 		type: 'POST',
 		url: '/edittask',
 		data: `id=${id}&stat=${stat}&content=${content}&deadline=${deadline}`,
-		success: function() {
+		success: function(html) {
+			let task = document.getElementById(`${id}`);
+			task.innerHTML = html;
 		},
-		error: function() {
+		error: function(err) {
+			console.log(err);
 		}
 	});
-}
+};
 
 const newtask = function(e) {
+	let projid = document.getElementById("projid").value;
 	$.ajax({
 		type: 'POST',
 		url='/newtask',
 		data: `projid=${projid}`,
-		success:
-	})
+		success: function(id) {
+			let task = document.createElement("div");
+			task.id = id;
+			task.addEventListener("click", edittask);
+			document.getElementById("tasks").appendChild(task);
+			edittask(task);
+		}
+		error: function(err) {
+			console.log(`could not create new element: ${err}`);
+		}
+	});
+};
+
+var tasks = document.getElementsByTagName("div");
+for(var i = 0; i < tasks.length; i++) {
+	tasks[i].addEventListener("click", edittask);
+}
+document.getElementById("create").addEventListener("click", newtask);
