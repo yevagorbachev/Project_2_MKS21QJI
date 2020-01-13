c6c48263e8314d98d86a4efd5f25af0bfc0296d6
edoss00 <edoss00@stuy.edu>
2020-01-12 22:50:34 -0500

    fixed for merge

--- app.py
+++ app.py
@@ -10,20 +10,9 @@ from os import urandom
 app = Flask(__name__)
 app.secret_key = urandom(32)
 
-# @app.route('/')
-# def index():
-#     return render_template('_base.html')
-
-## run template tests
-@app.route("/")
-def root():
-    if ("userid" in session): #if signed in, go to profile
-        return redirect(url_for('profile'))
-    return redirect(url_for('sign')) #else go to sign-in options
-
-#welcome page with sign-in options
-@app.route("/welcome")
-def sign():
-    return render_template('base_prof.html')
+@app.route('/')
+def index():
+    return render_template('_base.html')
 
 app.run(debug=True)
+
